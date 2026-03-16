0395c407f8753d5efd9a56ef9cccb2619d12794c
ilafper <ivanaalfaya@gmail.com>
2026-03-16 20:08:35 +0100

    asdasdad

--- agenda.js
+++ agenda.js
@@ -14,6 +14,133 @@ function leeMenu(question) {
   });
 }
 
+// lista clientes sql
+
+async function listaClientesSQL() {
+  try {
+    const response = await fetch("http://localhost:5000/api/clientesql");
+    console.log("isis sql lista clientes");
+
+    const datos = await response.json();
+
+    const clientes = datos.lista_clientes;
+
+    for (let i = 0; i < clientes.length; i++) {
+      console.log(`--- Cliente SQL ${i + 1} ---`);
+      console.log(`Nombre: ${clientes[i].nombre}`);
+      console.log(`Apellidos: ${clientes[i].apellidos}`);
+      console.log(`Teléfono: ${clientes[i].telefono}`);
+      console.log(`Dirección: ${clientes[i].direccion}`);
+      console.log(`Correo: ${clientes[i].correo}`);
+      console.log(`Codifo Usuario: ${clientes[i].code_user}`);
+
+      console.log("--------------------\n");
+    }
+  } catch (error) {
+    console.error("Error al conectar con la API:", error);
+  }
+}
+
+
+
+async function crearUsuarioSQL() {
+  //console.log("sisis");
+
+  let nombre = await leeMenu("nombre usuario: ");
+  let apellidos = await leeMenu("Apellidos usuario: ");
+  let telefono = await leeMenu("Telefono usuario:  ");
+  let direccion = await leeMenu("direccion usuario: ");
+  let correo = await leeMenu("Correo usuario: ");
+  
+
+
+  //console.log(nombre, apellidos, telefono, direccion, correo);
+
+  try {
+    const response = await fetch("http://localhost:5000/api/crearclientsql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre,
+        apellidos,
+        telefono,
+        direccion,
+        correo,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      console.log(data.mensaje);
+      
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+//borrar usuaio sql 
+
+async function borrarClienteSQL() {
+  const response = await fetch("http://localhost:5000/api/clientesql");
+  const datos = await response.json();
+  const clientes = datos.lista_clientes;
+
+  for (let i = 0; i < clientes.length; i++) {
+    console.log(
+      `${i + 1}. `,
+      `Nombre: ${clientes[i].nombre}`,
+      `Apellidos: ${clientes[i].apellidos}`,
+      `Codigo Usuario: ${clientes[i].code_user}`,
+    );
+  }
+
+  let clienteSelec = await leeMenu("Selecciona cliente para borrar: ");
+
+  // ajustar la posicion
+  let indice = clienteSelec - 1;
+
+  let cliente = clientes[indice];
+
+  console.log("cliente seleccionado:", cliente);
+
+  let id_eliminar = cliente.code_user;
+
+  console.log("id eliminar:", id_eliminar);
+
+    try {
+      const response = await fetch(`http://localhost:5000/api/deleteclientesql/${id_eliminar}`, {
+         method:"DELETE"
+      })
+      const data = await response.json();
+      console.log(data.mensaje);
+      
+      
+   
+    } catch (error) {
+      return {
+        success: false,
+        error: error.response?.data.error || "Error eliminar",
+      };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function listaClientes() {
   try {
     const response = await fetch("http://localhost:3000/api/clientes");
@@ -337,10 +464,13 @@ async function menuSQL() {
     opcion = parseInt(await leeMenu("Seleccione opción: "));
     switch (opcion) {
       case 1:
+        await crearUsuarioSQL();
         break;
       case 2:
+        await borrarClienteSQL();
         break;
       case 3:
+        await listaClientesSQL();
         break;
       case 4:
         break;
